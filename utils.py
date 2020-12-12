9ab7f79f769e4fa02c9f7f012fa0d3bff052f9bb
Andy <hslau44@gmail.com>
2020-12-12 22:00:09 +0000

    update utils.py

--- utils.py
+++ utils.py
@@ -6,6 +6,15 @@ import tensorflow as tf
 from collections import Counter
 
 def major_vote(arr, impurity_threshold=0.01):
+    """ find the element that has the majority portion in the array, depending on the threshold
+    
+    Args:
+    arr: np.ndarray. The target array 
+    impurity_threshold: float. If array contain a portion of other elemnets and they are higher than the threshold, the function return element with the smallest portion.  
+    
+    Return:
+    result: the element that has the majority portion in the array, depending on the threshold 
+    """
     counter = Counter(list(arr.reshape(-1)))
     lowest_impurity = float(counter.most_common()[-1][-1]/arr.shape[0])
     if lowest_impurity > impurity_threshold:
@@ -15,6 +24,18 @@ def major_vote(arr, impurity_threshold=0.01):
     return result
 
 def slide_augmentation(features,labels,window_size,slide_size,skip_labels=None):
+    """ Data augmentation, create sample by sliding 
+    Arg:
+    features: numpy.ndarray. Must have same size as labels in the first axis 
+    labels: numpy.ndarray. Must have same size as features in the first axis
+    window_size: int. Create a sample with size equal to window_size
+    slide_size: int. Create a sample for each slide_size in the first axis 
+    skip_labels: list. The labels that should be skipped 
+    
+    Reuturn:
+    X: numpy.ndarray. Augmented features
+    y: np.ndarray. Augmented labels
+    """
     assert features.shape[0] == labels.shape[0]
     X,y,z = [],[],[]
     for i in range(0,len(features)-window_size+1,slide_size):
@@ -26,14 +47,18 @@ def slide_augmentation(features,labels,window_size,slide_size,skip_labels=None):
             y.append(label)
     return np.array(X),np.array(y)
 
-def stacking(data,scale=255):
-    if scale != False:
-        scaler = MinMaxScaler()
-        data_s = scale*scaler.fit_transform(data.reshape(len(data),-1))
-    else:
-        data_s = data
-    data_s = data_s.reshape(data_s.shape[0],-1,90,1) # change axis 1
-    return np.concatenate((data_s,data_s,data_s),axis=3)
+def stacking(x):
+    """Increase channel dimension from 1 to 3"""
+    
+#     if scale != False:
+#         scaler = MinMaxScaler()
+#         data_s = scale*scaler.fit_transform(data.reshape(len(data),-1))
+#     else:
+#         data_s = data
+#     data_s = data_s.reshape(data_s.shape[0],-1,90,1) # change axis 1
+#     return np.concatenate((data_s,data_s,data_s),axis=3)
+    x = x.reshape(*x.shape,1)
+    return np.concatenate((x,x,x),axis=3)
 
 def breakpoints(ls):
     points = []
@@ -45,6 +70,7 @@ def breakpoints(ls):
 from sklearn.utils import shuffle
 
 def index_resampling(arr,oversampling=True):
+    """Return a list of index after resampling from array"""
     series = pd.Series(arr.reshape(-1))
     value_counts = series.value_counts()
     if oversampling == True:
@@ -60,46 +86,34 @@ def index_resampling(arr,oversampling=True):
     return idx_ls
 
 def resampling(X,y,z,oversampling=True):
+    """Resampling argument"""
     idx_ls = index_resampling(y,oversampling)
     X,y,z = shuffle(X[idx_ls],y[idx_ls],z[idx_ls])
     return X,y,z
 
-# def scale_dataframe(df,scaler,fit=False):
-#     cols = df.columns[:-2]
-#     data = df.iloc[:,:-2].to_numpy()
-#     if fit == True:
-#         print("fit_dataframe")
-#         data = scaler.fit_transform(data)
-#     else:
-#         print("transform_dataframe")
-#         data = scaler.transform(data)
-#     return pd.concat([pd.DataFrame(data,columns=cols).reset_index(drop=True),
-#                       df.iloc[:,-2:].reset_index(drop=True)],axis=1),scaler
 
-def evaluation(model,X_test,y_test,ohe,return_all_record=False):
-    """
-    Print classification report and confusion matrix (cmtx) with categories in one-hot-encoder
+def evaluation(model,X_test,y_test,encoder,return_all_record=False):
+    """Print classification report and confusion matrix (cmtx) with categories in one-hot-encoder
     
-    Input
-        model (tensorflow.python.keras.engine.training.Model): tensorflow model
-        X_test (numpy.ndarray): test set feature
-        y_test (numpy.ndarray): test set label
-        ohe (sklearn.preprocessing.OneHotEncoder):
-        return_all_record (bool): 
+    Args:
+    model: tensorflow.python.keras.engine.training.Model. Tensorflow model
+    X_test: numpy.ndarray. test set feature
+    y_test: numpy.ndarray. test set label
+    encoder: sklearn.preprocessing._encoders
+    return_all_record: bool. If True, return encoded predicted result, actural result, classification_report and confusion matrix
         
     Return
-        cmtx (pd.DataFrame): confusion matrix
-    
+    cmtx: pandas.DataFrame. confusion matrix
     """
     model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
-    pred_c = ohe.inverse_transform(model.predict(X_test))
-    actual_c = ohe.inverse_transform(y_test)
-    report = classification_report(actual_c,pred_c,ohe.categories_[0].tolist())
-    print(classification_report(actual_c,pred_c,ohe.categories_[0].tolist()))
+    pred_c = encoder.inverse_transform(model.predict(X_test))
+    actual_c = encoder.inverse_transform(y_test)
+    report = classification_report(actual_c,pred_c,encoder.categories_[0].tolist())
+    print(classification_report(actual_c,pred_c,encoder.categories_[0].tolist()))
     cf_matrix = confusion_matrix(actual_c,pred_c)
     cmtx = pd.DataFrame(confusion_matrix(actual_c,pred_c),
-                        index=[f"actual: {i}"for i in ohe.categories_[0].tolist()], 
-                        columns=[f"predict : {i}"for i in ohe.categories_[0].tolist()])
+                        index=[f"actual: {i}"for i in encoder.categories_[0].tolist()], 
+                        columns=[f"predict : {i}"for i in encoder.categories_[0].tolist()])
     if return_all_record == True:
         return pred_c, actual_c, report, cmtx
     return cmtx
@@ -147,19 +161,18 @@ def quick_history(ls_tensors):
 
 
 def quick_eval(model,train,test,target_train=None,target_test=None):
-    """
-    Print and return model loss and accuracy during custom training. 
+    """Print and return model loss and accuracy during custom training. 
     
     input:
-        model (tensorflow.python.keras.engine.training.Model): model 
-        train (tuple<numpy.ndarray>): tuple of array querred from DatasetObject 
-        test (tuple<numpy.ndarray>): tuple of array querred from DatasetObject
-        target_train (tuple<numpy.ndarray>):
-        target_test (tuple<numpy.ndarray>):
+    model: tensorflow.python.keras.engine.training.Model. model 
+    train: list<numpy.ndarray>. tuple of array querred from DatasetObject 
+    test: list<numpy.ndarray>. tuple of array querred from DatasetObject
+    target_train (tuple<numpy.ndarray>). 
+    target_test (tuple<numpy.ndarray>). 
     
     return:
-        results (list<float>): model loss and accuracy
-        result_names (list<string>): 
+    results: list<float>). model loss and accuracy
+    result_names: list<string>). 
     
     """
     model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
